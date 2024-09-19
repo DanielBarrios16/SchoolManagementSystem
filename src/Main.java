d77a0c3286ac41d33c6bc163bb66b238b5a75eb3
DanielBarrios16 <danielsay573@gmail.com>
2024-09-19 11:06:01 -0600

    DanielParte2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Estudiante estudiante1 = new Estudiante(1, "Juan", "Pérez", "2000-01-01", Estudiante.Estado.MATRICULADO);
+        Estudiante estudiante2 = new Estudiante(2, "María", "Gómez", "1999-05-23", Estudiante.Estado.MATRICULADO);
+
+
+        Curso curso1 = new Curso(1, "Matemáticas", "Curso de álgebra y cálculo", 3, "1.0");
+        Curso curso2 = new Curso(2, "Historia", "Historia Universal", 2, "1.0");
+
+
+        GestorAcademico gestor = new GestorAcademico();
+
+        try {
+
+            gestor.matricularEstudiante(estudiante1);
+            gestor.matricularEstudiante(estudiante2);
+
+
+            gestor.agregarCurso(curso1);
+            gestor.agregarCurso(curso2);
+
+
+            gestor.inscribirEstudianteCurso(estudiante1, curso1.getId());
+            gestor.inscribirEstudianteCurso(estudiante2, curso2.getId());
+
+
+            gestor.desinscribirEstudianteCurso(estudiante1.getId(), curso1.getId());
+
+
+            gestor.inscribirEstudianteCurso(estudiante1, curso1.getId());
+        } catch (EstudianteYaInscritoException | EstudianteNoInscritoEnCursoException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
